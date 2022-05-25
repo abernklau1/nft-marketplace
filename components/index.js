caded20343fb843d2875f604817e9c1d3698e23f
abernklau1 <andrewbernklau@hotmail.com>
2022-05-25 17:55:12 -0600

    Finished Home page

---
+++ components/index.js
@@ -0,0 +1,16 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
+
+export {
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  EthPrice,
+  NFTTitle,
+};
